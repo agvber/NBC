e08ba3e0531759f12d617c4fa52e24ea08d75156
agvber <agvber12@gmail.com>
2024-03-01 14:33:40 +0900

    Add: BaseActivity 추가

--- app/src/main/java/com/agvber/mbti_test/MainActivity.kt
+++ app/src/main/java/com/agvber/mbti_test/MainActivity.kt
@@ -1,15 +1,9 @@
 package com.agvber.mbti_test
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.agvber.mbti_test.ui.theme.MBTI_TEST_Theme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +11,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MBTI_TEST_Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+               val intent = Intent(this, BaseActivity::class.java)
+                startActivity(intent)
+                finish()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MBTI_TEST_Theme {
-        Greeting("Android")
     }
 }
