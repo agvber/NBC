146453d6b572ec672fe16ae80a42508aea210d0e
agvber <agvber12@gmail.com>
2024-03-05 18:15:14 +0900

    Add: 계산 기능 완료

---
+++ app/src/main/java/com/agvber/calculator/Calculator.kt
@@ -0,0 +1,20 @@
+package com.agvber.calculator
+
+class Calculator {
+
+    fun add(number: Long, other: Long): Long {
+        return number + other
+    }
+
+    fun minus(number: Long, other: Long): Long {
+        return number - other
+    }
+
+    fun divide(number: Long, other: Long): Long {
+        return number / other
+    }
+
+    fun times(number: Long, other: Long): Long {
+        return number * other
+    }
+}
