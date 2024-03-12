b90dc85b599ddd869ce382cac2e3100cc45cce1b
agvber <agvber12@gmail.com>
2024-03-12 15:42:55 +0900

    Add: 버거 메뉴 view 추가

--- app/src/main/java/com/agvber/kiosk/view/MenuView.kt
+++ app/src/main/java/com/agvber/kiosk/view/MenuView.kt
@@ -12,4 +12,16 @@ class MenuView {
                     "0. 종료            | 프로그램 종료"
         )
     }
+
+    fun printBurgersMenu() {
+        println(
+            "[ Burgers MENU ]\n" +
+                    "1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거\n" +
+                    "2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거\n" +
+                    "3. Shroom Burger | W 9.4 | 몬스터 치즈와 체다 치즈로 속을 채운 베지테리안 버거\n" +
+                    "3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거\n" +
+                    "4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거\n" +
+                    "0. 뒤로가기      | 뒤로가기"
+        )
+    }
 }
