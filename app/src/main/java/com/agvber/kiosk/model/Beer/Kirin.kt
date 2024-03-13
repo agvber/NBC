ae11f02b371e583d491783235fa6621d5e4b85ff
agvber <agvber12@gmail.com>
2024-03-13 15:22:54 +0900

    Add: 맥주 클래스 추가

---
+++ app/src/main/java/com/agvber/kiosk/model/Beer/Kirin.kt
@@ -0,0 +1,11 @@
+package com.agvber.kiosk.model.Beer
+
+class Kirin {
+
+    val name: String = "Kirin"
+    val price: Int = 29000
+
+    fun displayInfo(): String {
+        return "라벨을 보면 맛이 보인다 보리 100%의 첫즙만 담은 단 하나의 맛"
+    }
+}
