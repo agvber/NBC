a178169a9b7d14d92b87af8a43f1d16f69e54f89
agvber <agvber12@gmail.com>
2024-03-13 15:21:24 +0900

    Add: 음료 클래스 추가

---
+++ app/src/main/java/com/agvber/kiosk/model/drink/Fanta.kt
@@ -0,0 +1,11 @@
+package com.agvber.kiosk.model.drink
+
+class Fanta {
+
+    val name: String = "Fanta"
+    val price: Int = 29000
+
+    fun displayInfo(): String {
+        return "환타의 상큼하고 재미있는 맛을 만나보세요. 환타 파인애플, 오렌지, 포도, 밀크소다와 같은 클래식 소프트 드링크가 모든 연령층에게 사랑받고 있습니다."
+    }
+}
---
+++ app/src/main/java/com/agvber/kiosk/model/drink/Sprite.kt
@@ -0,0 +1,11 @@
+package com.agvber.kiosk.model.drink
+
+class Sprite {
+
+    val name: String = "Sprite"
+    val price: Int = 39000
+
+    fun displayInfo(): String {
+        return "입안 가득 퍼지는 상쾌함과 시원함을 을 느낄 수 있는 스프라이트는 전세계 190개국 이상에서 판매되며 쿨하고 힙한 매력으로 사랑 받고 있습니다."
+    }
+}
