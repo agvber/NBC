663cd83720780dc84a2a46d20cb71ca7363f0d90
agvber <agvber12@gmail.com>
2024-03-13 15:15:46 +0900

    Add: 버거 클래스 추가

---
+++ app/src/main/java/com/agvber/kiosk/model/burger/Hamburger.kt
@@ -0,0 +1,11 @@
+package com.agvber.kiosk.model.burger
+
+class Hamburger {
+
+    val name: String = "Hamburger"
+    val price: Int = 54000
+
+    fun displayInfo(): String {
+        return "비프패티를 기반으로 야채가 들어간 기본버거"
+    }
+}
---
+++ app/src/main/java/com/agvber/kiosk/model/burger/ShroomBurger.kt
@@ -0,0 +1,11 @@
+package com.agvber.kiosk.model.burger
+
+class ShroomBurger {
+
+    val name: String = "Shroom Burger"
+    val price: Int = 94000
+
+    fun displayInfo(): String {
+        return "몬스터 치즈와 체다 치즈로 속을 채운 베지테리안 버거"
+    }
+}
