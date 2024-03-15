f3a96b9c5b90bdaf5c5788c2e438072345739259
agvber <agvber12@gmail.com>
2024-03-15 11:52:44 +0900

    Feat: 현재 잔액을 나타내는 ui를 추가 하였습니다

--- app/src/main/java/com/agvber/kiosk/view/OutputView.kt
+++ app/src/main/java/com/agvber/kiosk/view/OutputView.kt
@@ -10,4 +10,8 @@ class OutputView {
         println("번호를 잘못 입력하셨습니다")
         println("다시 입력하여주세요.")
     }
+
+    fun printMoney(money: Int) {
+        println("현재 잔액은: ${money}원 입니다.")
+    }
 }
