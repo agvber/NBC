979116a21537611cbd5430db94dd250e95b154a3
agvber <agvber12@gmail.com>
2024-03-15 11:44:11 +0900

    Feat: 메뉴판 번호를 잘못 입력시에 출력되는 에러 문구 추가

--- app/src/main/java/com/agvber/kiosk/view/OutputView.kt
+++ app/src/main/java/com/agvber/kiosk/view/OutputView.kt
@@ -5,4 +5,9 @@ class OutputView {
     fun shutdownProgram() {
         println("프로그램을 종료합니다.")
     }
+
+    fun printNumberError() {
+        println("번호를 잘못 입력하셨습니다")
+        println("다시 입력하여주세요.")
+    }
 }
