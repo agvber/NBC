b3d5523a67f1fcd7a6acd4a350cf293c2672fa9a
agvber <agvber12@gmail.com>
2024-03-19 11:46:40 +0900

    Add: SignInActivity 값을 받아서 HomeActivity에 전달하는 로직 추가

--- app/src/main/java/com/agvber/my_info/SignInActivity.kt
+++ app/src/main/java/com/agvber/my_info/SignInActivity.kt
@@ -1,18 +1,39 @@
 package com.agvber.my_info
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.agvber.my_info.model.User
 
 class SignInActivity : AppCompatActivity() {
+
+    private var user: User? = null
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        val id = it.data?.extras?.getString(HomeActivity.ID)
+        val password = it.data?.extras?.getString(HomeActivity.PASSWORD)
+        val name = it.data?.extras?.getString(HomeActivity.NAME)
+        val age = it.data?.extras?.getInt(HomeActivity.AGE)
+        val mbti = it.data?.extras?.getString(HomeActivity.MBTI)
+
+        user = User(
+            id = id ?: "",
+            password = password ?: "",
+            name = name ?: "",
+            age = age ?: 0,
+            mbti = mbti ?: ""
+        )
+        Log.d("user", "$user")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
-
 
         val idEditText = findViewById<EditText>(R.id.id_edit_text)
         val passwordEditText = findViewById<EditText>(R.id.password_edit_text)
@@ -25,12 +46,12 @@ class SignInActivity : AppCompatActivity() {
                 showLoginFailedToast()
                 return@setOnClickListener
             }
-            startHomeActivity(idEditText.text.toString(), passwordEditText.text.toString())
+            startHomeActivity()
             showLoginSuccessToast()
         }
 
         registerButton.setOnClickListener {
-            startSignUpActivity(idEditText.text.toString(), passwordEditText.text.toString())
+            startSignUpActivity()
         }
     }
 
@@ -42,19 +63,19 @@ class SignInActivity : AppCompatActivity() {
         Toast.makeText(this, "로그인 성공", Toast.LENGTH_SHORT).show()
     }
 
-    private fun startHomeActivity(id: String, password: String) {
+    private fun startHomeActivity() {
         val intent = Intent(this, HomeActivity::class.java).apply {
-            putExtra("id", id)
-            putExtra("password", password)
+            putExtra(HomeActivity.ID, user?.id)
+            putExtra(HomeActivity.PASSWORD, user?.password)
+            putExtra(HomeActivity.NAME, user?.name)
+            putExtra(HomeActivity.AGE, user?.age)
+            putExtra(HomeActivity.MBTI, user?.mbti)
         }
         startActivity(intent)
     }
 
-    private fun startSignUpActivity(id: String, password: String) {
-        val intent = Intent(this, SignUpActivity::class.java).apply {
-            putExtra("id", id)
-            putExtra("password", password)
-        }
-        startActivity(intent)
+    private fun startSignUpActivity() {
+        val intent = Intent(this, SignUpActivity::class.java)
+        launcher.launch(intent)
     }
 }
