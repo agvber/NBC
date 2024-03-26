15b9d3748296efe7e29ba14e13945553e7ed72c2
agvber <agvber12@gmail.com>
2024-03-26 15:09:22 +0900

    Refactor: mvvm 패턴으로 리팩토링

--- app/src/main/java/com/agvber/my_info/SignUpActivity.kt
+++ app/src/main/java/com/agvber/my_info/SignUpActivity.kt
@@ -4,9 +4,14 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 
 class SignUpActivity : AppCompatActivity() {
+
+    private val viewModel: SignUpViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
@@ -14,30 +19,27 @@ class SignUpActivity : AppCompatActivity() {
         val nameEditText = findViewById<EditText>(R.id.name_edit_text)
         val idEditText = findViewById<EditText>(R.id.id_edit_text)
         val passwordEditText = findViewById<EditText>(R.id.password_edit_text)
-
         val registerButton = findViewById<Button>(R.id.register_button)
 
+        nameEditText.addTextChangedListener { viewModel.updateName(it.toString()) }
+        idEditText.addTextChangedListener { viewModel.updateId(it.toString()) }
+        passwordEditText.addTextChangedListener { viewModel.updatePassword(it.toString()) }
+
+
         registerButton.setOnClickListener {
-            val isTextBlank = nameEditText.text.isBlank() || idEditText.text.isBlank() || passwordEditText.text.isBlank()
-            if (isTextBlank) {
+            if (!viewModel.buttonEnable.value) {
                 showErrorMessage()
                 return@setOnClickListener
             }
-
-            val intent = intent
-            intent.apply {
-                putExtra(HomeActivity.ID, idEditText.text.toString())
-                putExtra(HomeActivity.PASSWORD, passwordEditText.text.toString())
-                putExtra(HomeActivity.NAME, nameEditText.text.toString())
-                putExtra(HomeActivity.AGE, 24)
-                putExtra(HomeActivity.MBTI, "INFJ")
-            }
-            setResult(200, intent)
-            finish()
+            showSuccessMessage()
         }
     }
-    
+
     private fun showErrorMessage() {
         Toast.makeText(this, "입력되지 않은 정보가 있습니다", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun showSuccessMessage() {
+        Toast.makeText(this, viewModel.toastMessage.value, Toast.LENGTH_SHORT).show()
     }
 }
