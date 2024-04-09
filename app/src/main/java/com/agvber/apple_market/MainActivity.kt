717190c31568fa5a181f1272fb912dc7f59f8188
agvber <agvber12@gmail.com>
2024-04-09 16:01:06 +0900

    feat: 메인 화면 ui controller 추가

--- app/src/main/java/com/agvber/apple_market/MainActivity.kt
+++ app/src/main/java/com/agvber/apple_market/MainActivity.kt
@@ -5,16 +5,34 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.agvber.apple_market.data.PreviewDataBase
+import com.agvber.apple_market.data.PreviewDataSource
+import com.agvber.apple_market.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val db: PreviewDataSource = PreviewDataBase
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.mainRecyclerView.adapter = MainAdapter(db.getItem())
+        binding.mainRecyclerView.addItemDecoration(
+            DividerItemDecoration(
+                this,
+                LinearLayoutManager.VERTICAL
+            )
+        )
+        binding.mainRecyclerView.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
     }
 }
