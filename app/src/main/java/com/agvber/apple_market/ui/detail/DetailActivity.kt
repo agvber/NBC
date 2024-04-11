fbf0b17c44d52f58321f115267e149dbbfdfde75
agvber <60022205+agvber@users.noreply.github.com>
2024-04-11 10:59:56 +0900

    디테일 화면을 구현하였습니다 (#2)
    * style: 디테일 페이지에서 필요한 아이콘 추가
    
    * add: sdk 버전 별 Parcelable extras를 처리하기 위한 getCustomParcelableExtra 함수 추가
    
    * add: 디테일 화면 ui 추가
    
    * add: 디테일 화면 ui controller 추가

--- app/src/main/java/com/agvber/apple_market/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/agvber/apple_market/ui/detail/DetailActivity.kt
@@ -1,21 +1,45 @@
 package com.agvber.apple_market.ui.detail
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.agvber.apple_market.R
+import com.agvber.apple_market.databinding.ActivityDetailBinding
+import com.agvber.apple_market.model.Post
+import com.agvber.apple_market.ui.getCustomParcelableExtra
+import java.text.NumberFormat
 
 class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val numberFormat = NumberFormat.getNumberInstance()
+        val post = intent.getCustomParcelableExtra(POST_DATA, Post::class.java)
+
+        binding.backButtonImageView.setOnClickListener {
+            finish()
+        }
+
+        with(binding) {
+            itemImageView.setImageResource(post.image)
+            nameTextView.text = post.seller
+            addressTextView.text = post.address
+            priceTextView.text = "${numberFormat.format(post.price)}${getString(R.string.currency)}"
+            titleTextView.text = post.name
+            descriptionTextView.text = post.description
         }
     }
 
