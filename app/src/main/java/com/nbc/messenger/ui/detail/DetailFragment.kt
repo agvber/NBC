a6b9758d1c745f626071d407bab98fbf004d5845
agvber <60022205+agvber@users.noreply.github.com>
2024-04-25 20:41:54 +0900

    fix: 누락된 파일 수정

--- app/src/main/java/com/nbc/messenger/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/nbc/messenger/ui/detail/DetailFragment.kt
@@ -3,21 +3,16 @@ package com.nbc.messenger.ui.detail
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.provider.ContactsContract.Data
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.nbc.messenger.createNotificationChannel
 import com.nbc.messenger.data.DataSource
 import com.nbc.messenger.databinding.FragmentDatailBinding
 import com.nbc.messenger.model.ProfileImage
 import com.nbc.messenger.model.User
-import com.nbc.messenger.ui.main.ContactListFragment
 import com.nbc.messenger.showNumberSelectionDialog
-import com.nbc.messenger.ui.main.ContactListFragment
 
 private const val USER_MEMORY = "user"
 
